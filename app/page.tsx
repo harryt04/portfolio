8f63b72b3fb9e29d6838426e0e0c349501907cdc
Harry Thomas <hsthomas@verisk.com>
2024-08-22 15:03:14 -0600

    links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <>More wiki pages coming soon...</>;
+  return (
+    <>
+      {/* vertical and horizontal center */}
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Link href="/resume.pdf">My resume</Link>
+        <Link href="https://github.com/harryt04?tab=repositories">
+          My Github
+        </Link>
+        <Link href="https://www.linkedin.com/in/harrison-thomas04/">
+          My LinkedIn
+        </Link>
+      </div>
+      More wiki pages coming soon...
+    </>
+  );
 }
