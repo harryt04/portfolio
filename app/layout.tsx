647400b4d4d3caf5b0cd87b0c378dce2551c79f7
Harry Thomas <hsthomas@verisk.com>
2024-08-22 15:28:12 -0600

    shadcn ui button and links on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,47 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+
+import { cn } from '@/lib/utils'
+import { PostHogProvider } from '@/providers/posthogProvider'
+import { ThemeProvider } from '@/providers/themeProvider'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
-  title: "Harry Thomas Wiki",
-  description: "Wiki for Full Stack Web & Mobile Deveoloper Harry Thomas",
-};
+  title: 'Harry Thomas Wiki',
+  description: 'Wiki for Full Stack Web & Mobile Deveoloper Harry Thomas',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <PostHogProvider>
+        <body
+          suppressHydrationWarning={true}
+          className={cn(
+            'min-h-screen bg-background font-sans antialiased',
+            fontSans.variable,
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </PostHogProvider>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,45 @@
-import Image from "next/image";
-import Link from "next/link";
+import { ThemeSwitcher } from '@/components/custom/themeSwitcher'
+import { Button } from '@/components/ui/button'
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       {/* vertical and horizontal center */}
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Link href="/resume.pdf">My resume</Link>
-        <Link href="https://github.com/harryt04?tab=repositories">
-          My Github
-        </Link>
-        <Link href="https://www.linkedin.com/in/harrison-thomas04/">
-          My LinkedIn
-        </Link>
+      <div className="flex h-screen flex-col items-center justify-center text-center">
+        <ThemeSwitcher />
+        <div className="grid w-full gap-4 p-4">
+          <p className="text-2xl font-bold">Hi, I&apos;m Harry Thomas</p>
+          <p className="text-m">
+            I&apos;m a full stack web and mobile developer
+          </p>
+          <Link href="/resume.pdf">
+            <Button className="w-48" variant="outline">
+              My resume
+            </Button>
+          </Link>
+          <Link
+            href="https://github.com/harryt04?tab=repositories"
+            target="_blank"
+          >
+            <Button className="w-48" variant="secondary">
+              <GitHubLogoIcon className="mr-2 h-4 w-4" />
+              My Github
+            </Button>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/harrison-thomas04/"
+            target="_blank"
+          >
+            <Button className="w-48" variant="secondary">
+              <LinkedInLogoIcon className="mr-2 h-4 w-4" />
+              My LinkedIn
+            </Button>
+          </Link>
+        </div>
       </div>
-      More wiki pages coming soon...
     </>
-  );
+  )
 }
