408a8a4dfab46294be5d78a0bf7cec479716c462
Harry Thomas <harrison.thomas04@gmail.com>
2026-09-10 20:50:35 -0400

    update theme switcher icons

--- components/custom/themeSwitcher.tsx
+++ components/custom/themeSwitcher.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { Monitor, Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
@@ -24,16 +24,23 @@ export function ThemeSwitcher() {
           aria-label="Choose color theme"
           className="h-11 w-11 border-border/90 bg-card text-foreground shadow-sm hover:border-accent hover:bg-muted hover:text-foreground"
         >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="light">
+            <Sun aria-hidden="true" className="mr-2 h-4 w-4" />
+            Light
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">
+            <Moon aria-hidden="true" className="mr-2 h-4 w-4" />
+            Dark
+          </DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="system">
-            Automatic
+            <Monitor aria-hidden="true" className="mr-2 h-4 w-4" />
+            System
           </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
       </DropdownMenuContent>
